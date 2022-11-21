339a33ef6089d076e1df242e8ecb53521041578d
katherings <katherinsibarani@gmail.com>
2022-11-21 21:24:55 +0700

    katherin g sibarani (21103093)unguided modul5 mobil

---
+++ Documents/NetBeansProjects/ProjectMobil/src/main/java/com/katherin/projectmobil/ProjectMobil.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.katherin.projectmobil;
+
+/**
+ *
+ * @author Katherin
+ */
+public class ProjectMobil {
+
+    public static void main(String[] args) {
+       System.out.println("sedan");
+
+        Sedan h = new Sedan();
+        h.tampilkan();
+
+        System.out.println("\nminibus");
+
+        Minibus t = new Minibus();
+        t.tampilkan();
+
+        System.out.println("\nBus");
+        
+        Bus z = new Bus();
+        z.tampilkan();
+    }
+}

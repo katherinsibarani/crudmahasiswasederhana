030948622d1d6fc69834bb9696fb45fded21f812
katherings <katherinsibarani@gmail.com>
2022-12-20 09:42:58 +0700

    katherin g sibarani (21103093)guided modul7 ibuanak

---
+++ Documents/NetBeansProjects/IbuAnak/src/main/java/com/katherin/ibuanak/IbuAnak.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.katherin.ibuanak;
+
+/**
+ *
+ * @author Katherin
+ */
+public class IbuAnak {
+
+    public static void main(String[] args) {
+        Manusia ibu1 = new Manusia("Budi", 30);
+        Manusia anak1 = new Manusia("Ani", 4);
+        Manusia ibu2 = new Manusia("Diana", 40 );
+        Manusia anak2 = new Manusia("Andi", 5, ibu2);
+        //Relasi antara Manusia dengan Manusia
+        System.out.println("=========================\n");
+        ibu1.cetak();
+        anak1.cetak();
+        System.out.println("=========================\n");
+        ibu1.adopsi(anak1);
+        ibu1.cetak();
+        anak1.cetak();
+        System.out.println("=========================\n");
+        ibu2.cetak();
+        anak2.cetak();  
+    }
+}
